324353057f1cab14210d36f6cee3cf385e538460
esra-er <er.esragul@gmail.com>
2022-11-20 14:15:27 +0100

    functions added

--- functions.js
+++ functions.js
@@ -1,10 +1,41 @@
-const findTheNumberOfOddNumbers = () => {};
+const findTheNumberOfOddNumbers = (pArray) => {
+  const oddNumbers=[];
+  pArray.map(number => {
+    if (number%2==1) {
+      oddNumbers.push(number)
+    }
+  })
+  return oddNumbers.length
+};
 
-const getIndexOfFalseItem = () => {};
+//----------------------------------------------------
 
-const filterNumbers = () => {};
+const getIndexOfFalseItem = (pArray) => {
+  let IndexOfFalse= pArray.indexOf(false)
+  return IndexOfFalse
+ };
+ 
+//-----------------------------------------------------
 
-const generateEmailAddress = () => {};
+const filterNumbers = (pArray) => {
+  const numbers=[];
+  pArray.map((value) => {
+    if(typeof value == "number"){
+      numbers.push(value)
+    }
+  })
+   return numbers
+  };
+
+//----------------------------------------------------
+
+const generateEmailAddress = (pOldMails) => {
+  let newMails = [];
+  pOldMails.map((value)=>{
+    newMails.push(value.toLowerCase()+"@gmail.com")
+  })
+  return newMails
+};
 
 export {
   findTheNumberOfOddNumbers,
